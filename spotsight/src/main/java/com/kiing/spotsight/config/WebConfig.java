f09dcd04196bdff223d1e6d587ab48b0229cfd4a
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-24 22:51:19 +0100

    web client config file created

---
+++ spotsight/src/main/java/com/kiing/spotsight/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.kiing.spotsight.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebConfig {
+    
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
