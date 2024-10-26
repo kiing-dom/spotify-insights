290322ab76c3a855559d7648f0be0e50963bf059
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-26 17:48:53 +0100

    finished set up of mock data

--- spotsight/src/test/java/com/kiing/spotsight/service/auth/SpotifyAuthServiceTest.java
+++ spotsight/src/test/java/com/kiing/spotsight/service/auth/SpotifyAuthServiceTest.java
@@ -1,8 +1,11 @@
 package com.kiing.spotsight.service.auth;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 public class SpotifyAuthServiceTest {
@@ -20,5 +23,11 @@ public class SpotifyAuthServiceTest {
 
     private WebClient.Builder webClientBuilder;
 
-    
+    @BeforeEach
+    public void setUp() {
+        WebClient webClientMock = mock(WebClient.class);
+        WebClient.RequestBodyUriSpec uriSpecMock = mock(WebClient.RequestBodyUriSpec.class);
+        WebClient.RequestHeadersSpec<?> headersSpecMock = mock(WebClient.RequestHeadersSpec.class);
+        WebClient.ResponseSpec responseSpecMock = mock(WebClient.ResponseSpec.class);
+    }
 }
