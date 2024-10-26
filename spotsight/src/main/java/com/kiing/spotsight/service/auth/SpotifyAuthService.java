ec10a392604ac643b6a480609d3e3ab111cbf7a8
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-26 12:39:03 +0100

    updated application properties | (need to change how response is handled in auth service now)

--- spotsight/src/main/java/com/kiing/spotsight/service/auth/SpotifyAuthService.java
+++ spotsight/src/main/java/com/kiing/spotsight/service/auth/SpotifyAuthService.java
@@ -1,5 +1,6 @@
 package com.kiing.spotsight.service.auth;
 
+
 import java.util.Base64;
 
 import org.springframework.stereotype.Service;
@@ -17,15 +18,10 @@ public class SpotifyAuthService {
     }
     
     public Mono<String> getAccessToken(String clientId, String clientSecret) {
-        String auth = Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes());
+        String authHeader = "Basic " +  Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes());
 
         return webClient.post()
-            .uri("/api/token")
-            .header("Authorization", "Basic " + auth)
-            .bodyValue("grant_type=client_credentials")
-            .retrieve()
-            .bodyToMono(String.class)
-            .map(response -> parseAccessToken(response));
+            
     }
 
     private String parseAccessToken(String response) {
