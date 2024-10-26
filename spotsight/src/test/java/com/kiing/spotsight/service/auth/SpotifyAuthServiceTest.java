c6a12a9bcb2453e5145a28f8168a3fcdada6ec31
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-26 18:24:15 +0100

    mocking webclient responses to TokenResponse objects

--- spotsight/src/test/java/com/kiing/spotsight/service/auth/SpotifyAuthServiceTest.java
+++ spotsight/src/test/java/com/kiing/spotsight/service/auth/SpotifyAuthServiceTest.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import static org.mockito.Mockito.mock;
+import com.kiing.spotsight.model.token.TokenResponse;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 public class SpotifyAuthServiceTest {
@@ -29,5 +32,18 @@ public class SpotifyAuthServiceTest {
         WebClient.RequestBodyUriSpec uriSpecMock = mock(WebClient.RequestBodyUriSpec.class);
         WebClient.RequestHeadersSpec<?> headersSpecMock = mock(WebClient.RequestHeadersSpec.class);
         WebClient.ResponseSpec responseSpecMock = mock(WebClient.ResponseSpec.class);
+
+        // configure mocks for web client methods
+        when(webClientMock.post()).thenReturn(uriSpecMock);
+        when(uriSpecMock.uri(tokenUrl)).thenReturn(uriSpecMock);
+        when(uriSpecMock.header(any(), any())).thenReturn(uriSpecMock);
+        when(uriSpecMock.bodyValue(any())).thenReturn(headersSpecMock);
+        when(headersSpecMock.retrieve()).thenReturn(responseSpecMock);
+
+        // mock the response of WebClient to return a TokenResponse object
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setAccessToken("mock-access-token");
+        tokenResponse.setTokenType("Bearer");
+        tokenResponse.setExpiresIn(3600);
     }
 }
