2e43c87d24f59595c60981f31622d8ed6f8e6299
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-26 12:12:39 +0100

    auth service get access token

--- spotsight/src/main/java/com/kiing/spotsight/service/auth/SpotifyAuthService.java
+++ spotsight/src/main/java/com/kiing/spotsight/service/auth/SpotifyAuthService.java
@@ -1,7 +1,11 @@
 package com.kiing.spotsight.service.auth;
 
+import java.util.Base64;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @Service
 public class SpotifyAuthService {
@@ -12,4 +16,19 @@ public class SpotifyAuthService {
         this.webClient = webClientBuilder.baseUrl("https://accounts.spotify.com").build();
     }
     
+    public Mono<String> getAccessToken(String clientId, String clientSecret) {
+        String auth = Base64.getEncoder().encodeToString((clientId + ":" + clientSecret.getBytes()));
+
+        return webClient.post()
+            .uri("/api/token")
+            .header("Authorization", "Basic " + auth)
+            .bodyValue("grant_type=client_credentials")
+            .retrieve()
+            .bodyToMono(String.class)
+            .map(response -> parseAccessToken(response));
+    }
+
+    private String parseAccessToken(String response) {
+        
+    }
 }
