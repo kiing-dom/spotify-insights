caf8922f622b293f09b5901116c85568ff97deae
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-29 15:45:11 +0000

    created UserProfile model class to parse calls from Spotify user endpoint to retrieve profile information

--- spotsight/src/main/java/com/kiing/spotsight/model/user/UserProfile.java
+++ spotsight/src/main/java/com/kiing/spotsight/model/user/UserProfile.java
@@ -1,5 +1,80 @@
 package com.kiing.spotsight.model.user;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 public class UserProfile {
+    @JsonProperty("display_name")
+    private String displayName;
     
+    private String email;
+
+    private String country;
+
+    private String id;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("images")
+    private List<Image> images;
+
+    public static class Image {
+        private String url;
+        private int height;
+        private int width;
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+    }
 }
