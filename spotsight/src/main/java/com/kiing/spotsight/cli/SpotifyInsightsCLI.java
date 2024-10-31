b9a4dbbb331de0c6ae42a3b2fac33d9142ad02d2
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-31 22:29:50 +0000

    base set up for cli testing of endpoints

---
+++ spotsight/src/main/java/com/kiing/spotsight/cli/SpotifyInsightsCLI.java
@@ -0,0 +1,30 @@
+package com.kiing.spotsight.cli;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SpotifyInsightsCLI {
+    
+    private static final String BASE_URL = "http://localhost:8080/api/";
+
+    public static void main(String[] args) throws Exception {
+        SpotifyInsightsCLI cli = new SpotifyInsightsCLI();
+        cli.run();
+    }
+
+    public void run() throws Exception {
+        System.out.println("Spotify Insights CLI");
+        System.out.println("1. Get User Profile");
+        System.out.println("2. Get Top Tracks");
+        System.out.println("Choose an Option: ");
+
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
+        int choice = Integer.parseInt(reader.readLine());
+
+        switch (choice) {
+            
+        }
+    }
+}
