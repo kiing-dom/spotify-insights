114f14beed43be0c2360f10711694342cb25f8c0
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-03 22:57:29 +0000

    started on getTopArtists endpoint

--- spotsight/src/main/java/com/kiing/spotsight/service/user/SpotifyUserService.java
+++ spotsight/src/main/java/com/kiing/spotsight/service/user/SpotifyUserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.kiing.spotsight.model.user.UserProfile;
+import com.kiing.spotsight.model.user.top.TopArtists;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -33,6 +34,6 @@ public class SpotifyUserService {
     }
 
     public Flux<TopArtists> getTopArtists(String accessToken) {
-        
+
     }
 }
