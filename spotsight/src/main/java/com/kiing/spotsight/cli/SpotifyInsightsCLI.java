9498c59e01716296a4a447886c870efb47727af1
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-04 23:30:53 +0000

    update cli | can get top artists

--- spotsight/src/main/java/com/kiing/spotsight/cli/SpotifyInsightsCLI.java
+++ spotsight/src/main/java/com/kiing/spotsight/cli/SpotifyInsightsCLI.java
@@ -10,16 +10,19 @@ import java.util.Scanner;
 import org.springframework.stereotype.Component;
 
 import com.kiing.spotsight.service.auth.SpotifyAuthService;
+import com.kiing.spotsight.service.user.SpotifyUserService;
 
 @Component
 public class SpotifyInsightsCLI {
     
     private static final String BASE_URL = "http://localhost:8080/api/";
     private final SpotifyAuthService spotifyAuthService;
+    private final SpotifyUserService spotifyUserService;
     private final BufferedReader reader;
 
-    public SpotifyInsightsCLI(SpotifyAuthService spotifyAuthService) {
+    public SpotifyInsightsCLI(SpotifyAuthService spotifyAuthService, SpotifyUserService spotifyUserService) {
         this.spotifyAuthService = spotifyAuthService;
+        this.spotifyUserService = spotifyUserService;
         this.reader = new BufferedReader(new InputStreamReader(System.in));
     }
 
@@ -30,7 +33,8 @@ public class SpotifyInsightsCLI {
             System.out.println("Spotify Insights CLI");
             System.out.println("1. Authenticate");
             System.out.println("2. Get User Profile");
-            System.out.println("3. Exit");
+            System.out.println("3. Get Top Artists");
+            System.out.println("4. Exit");
             System.out.print("Choose an Option: ");
 
             int choice = Integer.parseInt(reader.readLine());
@@ -38,7 +42,8 @@ public class SpotifyInsightsCLI {
             switch (choice) {
                 case 1 -> authenticateUser();
                 case 2 -> getUserProfile();
-                case 3 -> {
+                case 3 -> getTopArtists();
+                case 4 -> {
                     System.out.println("Exiting...");
                     running = false;
                 }
@@ -73,30 +78,39 @@ public class SpotifyInsightsCLI {
             return; // Exit the method if no token is provided
         }
     
-        HttpURLConnection conn = (HttpURLConnection) new URL(userProfileEndpoint).openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
-        
-        int responseCode = conn.getResponseCode();
-        
+        spotifyUserService.getUserProfile(accessToken)
+            .doOnNext(user -> System.out.println("Got the user profile for " + user.getDisplayName()))
+            .doOnNext(user -> System.out.println("From Country: " + user.getCountry()))
+            .doOnError(e -> System.out.println("Error" + e.getMessage()))
+            .subscribe();
         // Check if the access token is valid
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            StringBuilder content = new StringBuilder();
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            
-            in.close();
-            System.out.println("User Profile Data: " + content.toString());
-        } else if (responseCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
-            System.out.println("Invalid access token. Please check your token and try again.");
-        } else {
-            System.out.println("Failed to fetch data: HTTP error code " + responseCode);
-        }
-    
+        
         
         // scanner.close();
+    }
+
+    private void getTopArtists()  {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the access token: ");
+        String accessToken = scanner.nextLine();
+
+        System.out.println("Enter time range (short_term, medium_term, long_term): ");
+        String timeRange = scanner.nextLine();
+
+        System.out.println("Enter Artist Limit (e.g. 10): ");
+        int limit = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter offset (e.g. 0): ");
+        int offset = Integer.parseInt(scanner.nextLine());
+
+        if(accessToken.isEmpty()) {
+            System.out.println("An access token is required. Please provide one");
+            return;
+        }
+
+        spotifyUserService.getTopArtists(accessToken, timeRange, limit, offset)
+            .doOnNext(artist -> System.out.println("Artists: " + artist.getName()))
+            .doOnError(e -> System.out.println("Error: " + e.getMessage()))
+            .subscribe();
     }
 }
