cc34fd95f427a6cb78871d755a392ad3f625c7e1
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-06 00:32:05 +0000

    added getter methods to retrieve the data when spotify api is called

--- spotsight/src/main/java/com/kiing/spotsight/model/user/top/TopTracks.java
+++ spotsight/src/main/java/com/kiing/spotsight/model/user/top/TopTracks.java
@@ -7,8 +7,24 @@ public class TopTracks {
     public static class Item {
         private String name;
         private int popularity;
+        private Album album;
+        private Artists artists;
 
-        
+        public String getName() {
+            return name;
+        }
+        public int getPopularity() {
+            return popularity;
+        }
+
+        public Album getAlbum() {
+            return album;
+        }
+
+        private Artists getArtists() {
+            return artists;
+        }
+
     }
 
     public static class Album {
@@ -16,12 +32,34 @@ public class TopTracks {
         private String albumType;
         private int totalTracks;
         private String releaseDate;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAlbumType() {
+            return albumType;
+        }
+
+        public int getTotalTracks() {
+            return totalTracks;
+        }
+
+        public String getReleaseDate() {
+            return releaseDate;
+        }
     }
 
     public static class Artists {
         private String name;
         private String type;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
     }
-
-
 }
