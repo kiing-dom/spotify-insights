9bdc1daba2c381724c0123200d0962be24b94c71
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-06 00:16:05 +0000

    filled out attributes for subclasses

--- spotsight/src/main/java/com/kiing/spotsight/model/user/top/TopTracks.java
+++ spotsight/src/main/java/com/kiing/spotsight/model/user/top/TopTracks.java
@@ -5,16 +5,23 @@ import java.util.*;
 public class TopTracks {
     
     public static class Item {
+        private String name;
+        private int popularity;
+
         
     }
 
     public static class Album {
-
+        private String name;
+        private String albumType;
+        private int totalTracks;
+        private String releaseDate;
     }
 
     public static class Artists {
-
+        private String name;
+        
     }
 
-    
+
 }
