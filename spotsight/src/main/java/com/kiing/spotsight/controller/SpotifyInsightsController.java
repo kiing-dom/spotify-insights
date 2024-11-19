a6e6dc34951e5fda0ee5f28ed607b3db23b50c7b
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-11-19 16:16:07 +0000

    spotify insights controller complete

---
+++ spotsight/src/main/java/com/kiing/spotsight/controller/SpotifyInsightsController.java
@@ -0,0 +1,43 @@
+package com.kiing.spotsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kiing.spotsight.service.auth.SpotifyAuthService;
+import com.kiing.spotsight.service.user.SpotifyUserService;
+
+@Controller
+public class SpotifyInsightsController {
+    
+    private final SpotifyAuthService spotifyAuthService;
+    private final SpotifyUserService spotifyUserService;
+
+    public SpotifyInsightsController(SpotifyAuthService spotifyAuthService, SpotifyUserService spotifyUserService) {
+        this.spotifyAuthService = spotifyAuthService;
+        this.spotifyUserService = spotifyUserService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/authenticate")
+    public String authenticate(Model model) {
+        String authUrl = spotifyAuthService.getAuthorizationUrl();
+        model.addAttribute("authUrl", authUrl);
+        return "authenticate";
+    }
+
+    @GetMapping("user-profile")
+    public String getUserProfile(@RequestParam("accessToken") String accessToken, Model model) {
+        spotifyUserService.getUserProfile(accessToken)
+            .doOnNext(user -> model.addAttribute("user", user))
+            .doOnError(e -> model.addAttribute("error", e.getMessage()))
+            .subscribe();
+
+        return "userProfile";
+    }
+}
